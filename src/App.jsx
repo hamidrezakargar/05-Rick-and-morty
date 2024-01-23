5c0871a783fb2d7df5a400aec42216c4486bc9db
hamidreza <hamidrezakargar44@gmail.com>
2024-01-23 17:23:01 +0330

    characterList =>;

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-
+import { allCharacters } from "../data/data";
 import "./App.css";
+import CharacterList from "./components/CharacterList";
 import Navbar from './components/Navbar';
+import CharacterDetail from "./components/characterDetail";
+
+
+
 
 function App() {
   return (
-    <Navbar/>
+    <div className="app">
+      <Navbar />
+      <div className="main">
+
+        <CharacterList allCharacters={allCharacters} />
+        <CharacterDetail />
+
+      </div>
+    </div>
   )
 }
 export default App;
