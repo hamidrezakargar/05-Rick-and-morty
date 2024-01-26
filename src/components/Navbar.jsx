e3502ca27c8864b46dab65a9ea39fa12f1040792
hamidreza <hamidrezakargar44@gmail.com>
2024-01-26 17:44:24 +0330

    props driling => :)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import { HeartIcon } from "@heroicons/react/24/outline"
-function Navbar() {
+function Navbar({ numOfResult }) {
     return (
         <nav className="navbar">
             <div className="navbar__logo">LOGO :)</div>
-            <input type="text" className="text-field" placeholder="search ..." />
-            <div className="navbar__result">found X characters</div>
+
+            <input 
+            type="text" 
+            className="text-field" 
+            placeholder="search ..."
+            />
+
+            <div className="navbar__result">
+                found { numOfResult } characters
+            </div>
+
             <button className="heart">
+
                 <HeartIcon className="icon" />
+
                 <span className="badge" >3</span>
+
             </button>
 
         </nav>
